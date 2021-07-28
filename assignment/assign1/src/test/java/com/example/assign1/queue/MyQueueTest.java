c616cd08ec20d155ca854d63d5ec7a62246bef1f
naekang <66901361+naekang@users.noreply.github.com>
2021-07-28 10:41:47 +0900

    ADD test_Add method

--- assignment/assign1/src/test/java/com/example/assign1/queue/MyQueueTest.java
+++ assignment/assign1/src/test/java/com/example/assign1/queue/MyQueueTest.java
@@ -1,11 +1,28 @@
 package com.example.assign1.queue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class MyQueueTest {
+
+    private MyQueue myQueue;
+
+    @BeforeEach
+    void init_Queue() {
+        this.myQueue = new MyQueue();
+    }
 
     @Test
     void test_Constructor() {
         MyQueue myQueue = new MyQueue();
+    }
+
+    @Test
+    void test_Add() {
+        myQueue.add(1);
+        int expectedValue = 1;
+        assertThat(1).isEqualTo(expectedValue);
     }
 }
