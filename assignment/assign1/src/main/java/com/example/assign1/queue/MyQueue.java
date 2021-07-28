5d1ccec82790297be1f1d538112202441306ed05
naekang <66901361+naekang@users.noreply.github.com>
2021-07-28 10:44:22 +0900

    FEAT main add method

--- assignment/assign1/src/main/java/com/example/assign1/queue/MyQueue.java
+++ assignment/assign1/src/main/java/com/example/assign1/queue/MyQueue.java
@@ -2,4 +2,18 @@ package com.example.assign1.queue;
 
 public class MyQueue {
 
+    private Node first;
+    private Node last;
+
+    public void add(int i) {
+        Node newData = new Node(i);
+
+        if (first == null) {
+            first = newData;
+            last = first;
+        } else {
+            last.setNextNode(newData);
+            last = newData;
+        }
+    }
 }
