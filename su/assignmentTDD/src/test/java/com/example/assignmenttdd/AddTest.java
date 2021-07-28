f42cf53f6befa7dea663cdcf10dc260efa969422
cokkum113 <84273370+cokkum113@users.noreply.github.com>
2021-07-28 15:30:28 +0900

    1stPreTest

---
+++ su/assignmentTDD/src/test/java/com/example/assignmenttdd/AddTest.java
@@ -0,0 +1,12 @@
+package com.example.assignmenttdd;
+
+import org.junit.jupiter.api.Test;
+
+public class AddTest
+{
+    @Test
+    public void main()
+    {
+        System.out.println("true = " + true);
+    }
+}
