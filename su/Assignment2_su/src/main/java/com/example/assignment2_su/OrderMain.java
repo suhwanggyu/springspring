75ee62513795ef96cd02c5dc61ed16fb6541cb3e
cokkum113 <84273370+cokkum113@users.noreply.github.com>
2021-08-03 17:24:16 +0900

    TDD for Make console

---
+++ su/Assignment2_su/src/main/java/com/example/assignment2_su/OrderMain.java
@@ -0,0 +1,7 @@
+package com.example.assignment2_su;
+
+public class OrderMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ su/Assignment2_su/src/main/java/com/example/assignment2_su/PizzaDao.java
@@ -0,0 +1,4 @@
+package com.example.assignment2_su;
+
+public class PizzaDao {
+}
