4a82c618422e8e37d8323a0182a196adb433c663
cokkum113 <84273370+cokkum113@users.noreply.github.com>
2021-08-03 16:09:44 +0900

    Prepare

---
+++ su/Assignment2_su/src/main/java/com/example/assignment2_su/Assignment2SuApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignment2_su;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment2SuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment2SuApplication.class, args);
+    }
+
+}
---
+++ su/Assignment2_su/src/test/java/com/example/assignment2_su/Assignment2SuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment2_su;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2SuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
